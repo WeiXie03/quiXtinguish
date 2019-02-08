a676b90275fe076d344b0cc91707ce88146231cf
Wei Xie <wei.tom.xie@gmail.com>
2019-02-07 22:24:00 -0800

    forgot to upload something

--- stereoSetup/receivewrite_orig.py
+++ stereoSetup/receivewrite_orig.py
@@ -2,45 +2,60 @@ import numpy
 import cv2
 import time
 
-cap0 = cv2.VideoCapture('udpsrc port=5200 ! application/x-rtp,media=video,payload=26,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink',cv2.CAP_GSTREAMER)
-#receive video packaged in RTP packets from Raspi over UDP
-cap1 = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp,media=video,payload=26,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink',cv2.CAP_GSTREAMER)
+class CamWin(cv2.namedWindow):
 
-#pre-define OpenGL windows for displaying streams
-win0 = cv2.namedWindow('right', cv2.WINDOW_OPENGL)
-win1 = cv2.namedWindow('left', cv2.WINDOW_OPENGL)
+    streams = []
+    imCount = 0
+    baseDist =
 
-imCount = 0
+    def __init__(self, camStream, side): #camStream is the cv2.VideoCapture object that represents a video stream
+        CamWin.streams.append(self)
+        self.side = side #side is simply 'left' or 'right'
+        self.writeDir = raw_input('Enter the path of the directory where you would like to save images(' + self.side + ' cam)')
+        super(CamWin, self).__init__(self.side, cv2.WINDOW_OPENGL)
 
-while(cap0.isOpened(),cap1.isOpened()):
-    # Capture frame-by-frame
-    ret0, frame0 = cap0.read()
-    ret1, frame1 = cap1.read()
+    def dispFrame(self, frame):
+        # Capture frame-by-frame
 
-    '''
-    # shrink the images to half size
-    frame0 = cv2.resize(frame0,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
-    frame1 = cv2.resize(frame1,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
-    '''
+        '''
+        # shrink the images to half size
+        frame0 = cv2.resize(frame0,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
+        frame1 = cv2.resize(frame1,None,fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
+        '''
 
-    #display the camera streams
-    cv2.imshow('right',frame0)
-    cv2.imshow('left',frame1)
+        #display the camera streams
+        cv2.imshow(self.side, frame)
 
-    keyHit = cv2.waitKey(1)
+    def clicked(self, event, x, y, flags, frame):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            cv2.circle(img,(x,y),100,(255,0,0),-1)
+            print('mouse coordinates(x, y): 'x, ',', y, '\tpoint depth = ', )
 
-    if keyHit & 0xFF == 32: #pass hexadecimal and ASCII code of the key pressed into an AND gate to only take the first 8 bits
-        #if space bar pressed, save frame as image file
-        cv2.imwrite('./roughDistTesting/right' + str(imCount) + '.jpg', frame0)
-        cv2.imwrite('./roughDistTesting/left' + str(imCount) + '.jpg', frame1)
-        print('written ', str(imCount + 1), ' images for each camera')
+            cv2.imwrite(self.writeDir + self.side + str(CamWin.imCount), frame)
+            imCount += 1
+            print('wrote ' + self.side + ' frame to ', self.writeDir)
 
-        imCount += 1
+if __name__ == "__main__":
+    #receive video packaged in RTP packets from gstreamer on Raspi over UDP
+    cap0 = cv2.VideoCapture('udpsrc port=5200 ! application/x-rtp,media=video,payload=26,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink',cv2.CAP_GSTREAMER)
+    cap1 = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp,media=video,payload=26,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink',cv2.CAP_GSTREAMER)
 
-    elif keyHit & 0xFF == ord('q'): #if user hits q, stop capturing
-        break
+    #create the window objects(class defined above)
+    win0 = CamWin(cap0, 'right')
+    win1 = CamWin(cap1, 'left')
 
-# When everything done, release the capture
+    while(True):
+        #loop through all camera streams
+        for stream in CamWin.streams:
+            isRec, frame = stream.cap.read()
+            stream.dispFrame(frame)
+            stream.setMouseCallback(stream.side, stream.clicked, frame)
+
+        if cv2.waitKey == ord('q'):
+            break
+            print('quit capturing')
+
+#When everything done, release the capture
 cap0.release()
 cap1.release()
 cv2.destroyAllWindows()
