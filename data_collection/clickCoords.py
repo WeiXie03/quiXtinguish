b890ab2a108bb7c157ca0089dbae8c4d9ab3fb51
Wei Xie <wei.tom.xie@gmail.com>
2019-02-12 08:17:23 -0800

    clicking labeller almost working

--- data_collection/clickCoords.py
+++ data_collection/clickCoords.py
@@ -1,5 +1,5 @@
 import cv2
-import numpy
+import numpy as np
 import pickle
 import os
 import pdb
@@ -8,51 +8,93 @@ import sys
 #pdb.set_trace()
 
 class Cam(object):
-    def __init__(self, side, img_path):
+    def __init__(self, side):
         self.side = side
-        self.win = cv2.namedWindow(self.side, cv2.WINDOW_OPENGL)
+        self.win = cv2.namedWindow(self.side, cv2.WINDOW_AUTOSIZE)
+        self.img = None
 
-        self.img = cv2.imread(img_path) #get img_path from metadata
+def clicked(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        src = param[1]
+        coord_dict = param[0]
+        print('x: ', x, ', y: ', y)
+        coord_dict["x"] = x
+        coord_dict["y"] = y
+        print('selected')
+        disp_img = cv2.circle(src.img, (x,y), 6, (0,15,254), 2)
+        cv2.imshow(src.side, disp_img)
 
-#def saveDat(x, y):
-    #saves coordinates of mouse, disparity and calculated depth to json file
+def waitClick(src):
+    print('waiting click')
+    coord_dict = {'x':None, 'y':None}
+    cv2.setMouseCallback(src.side, clicked, (coord_dict, src))
+    print('hit w to confirm your selection, q at any time to exit')
+    while True:
+        key = cv2.waitKey(1)
+        if(key == 32):
+            break
+        elif(key == ord('q')):
+            sys.exit()
+    return coord_dict['x'], coord_dict['y']
 
 if __name__ == "__main__":
-    def clicked(event, x, y, flags, param):
-        param[0] = src
-        param[1] = metadata
-        if event == cv2.EVENT_LBUTTONDOWN:
-            print('x: ', x, ', y: ', y)
-            metadata[src.side]['coords'] = (x, y)
-            print('wrote coords to metadata')
-
     DATA_DIR = 'data/camsSetPerm_channel/'
 
+    #load the metadata
     metadata_path = os.path.join(DATA_DIR, 'metadata.dat')
+    metadata = None
     if os.path.isfile(metadata_path):
-        metadataf = open(metadata_path, 'rb')
-        metadata = pickle.load(metadataf)
+        with open(metadata_path, 'rb') as metadataf:
+            metadata = pickle.load(metadataf)
     else:
-        print('the file ', metadata_path, ' was not found\nplease terminate this program and run receivewrite2.py')
+        print('the file ', metadata_path, ' was not found\nplease run receivewrite2.py')
         sys.exit()
-    #metadata.sort()
 
-    print(metadata)
-    imNum = input('Enter the image number of the left right image pair you would like to use.\n ')
-    pair_path = metadata[imNum]
+    #get the image pair the user wants
+    left = Cam('left')
+    right = Cam('right')
 
-    left = Cam('left', pair_path['left'])
-    right = Cam('right', pair_path['right'])
+    while(True):
+        print(metadata)
+        imNum = (input('Enter the image number of the left right image pair you would like to use or enter \'w\' if you would like to finish and save to file.\t')
+)
+        if imNum == 'w':
+            print('saving selections to metadata, continuing')
+            break
+        else:
+            imNum = int(imNum)
 
-    for src in [left, right]:
-        cv2.setMouseCallback(self.side, clicked, (src, metadata))
+        paird = metadata[imNum]
 
-        cv2.imshow(src.side, src.img)
-        while(len(metadata[src.side]) == 1): #should only include image location at this point
-            keyHit = cv2.waitKey(1)
-            if keyHit == ord('q'):
-                break
+        paird[left.side]['coords'] = {}
+        paird[right.side]['coords'] = {}
 
+        limg_path = paird[left.side]['img_path']
+        left.img = cv2.imread(limg_path) #get img_path from metadata
+        rimg_path = paird[right.side]['img_path']
+        right.img = cv2.imread(rimg_path) #get img_path from metadata
+
+        print('Look at how many fires there are.')
+        cv2.imshow("both", np.hstack((left.img[::2, ::2],right.img[::2, ::2])))
+        cv2.waitKey(0)
+
+        numFires = int(input('Enter the number of fires in the image pair: '))
+        print('hit any key to continue')
+
+        print('Click on a fire in the image. When you are done, a window will pop up for you to select the corresponding point on the other image.')
+        for index in range(numFires):
+            for src in [left, right]:
+                #create new image field of wrappers
+
+                #load image into window, get number of fires from user
+                cv2.imshow(src.side, src.img)
+                mx,my = waitClick(src)
+                #store coordinates as values to index keys, will store points of corresponding left right images with same key
+                paird[src.side]['coords'][index] = (mx, my)
+
+        metadata[int(imNum)] = paird
+
+'''
     def writeIntri(metadata_intri, baseline, focl, disparity, est_depth):
         metadata_intri['baseline'] = baseline
         metadata_intri['focal length'] = focl
@@ -64,7 +106,7 @@ if __name__ == "__main__":
 
     CALIB_PATH = "./calibration/camMtx.npy"
     if os.path.isfile(CALIB_PATH):
-        calib = numpy.load(CALIB_PATH)
+        calib = np.load(CALIB_PATH)
     focl = calib[4] #focal length y from stereo calibration, refer to https://docs.opencv.org/3.1.0/dc/dbb/tutorial_py_calibration.html
     baseline = float(input('Enter distance between center of camera lenses in cm.\t'))
     disparity = metadata[imNum]['left']['coords'] - metadata[imNum]['right']['coords'] #disparity = left x - right x
@@ -73,10 +115,11 @@ if __name__ == "__main__":
 
     metadata[imNum] = writeIntri(metadata[imNum], baseline, focl, disparity, est_depth)
 
-    print(metadata)
-    if input('Would you like to save to file?[y/n]: ') == 'y':
-        pickle.dump(metadata, metadataf)
-    else:
-        print('aborting')
-    metadataf.close()
-    cv2.destroyAllWindows()
+'''
+print(metadata)
+if input('Would you like to save to file?[y/n]: ') == 'y':
+    with open(metadata_path, 'wb') as metadataf:
+        metadata = pickle.load(metadataf)
+else:
+    print('aborting')
+cv2.destroyAllWindows()
