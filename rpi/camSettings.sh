5ddbe97bf4bc9e9fa65ab30d20f5446887c9bcb7
Wei Xie@RPi <wei.tom.xie@gmail.com>
2019-02-16 20:05:05 -0800

    deleted stereoSetup, website configuration

---
+++ rpi/camSettings.sh
@@ -0,0 +1,33 @@
+v4l2-ctl -d /dev/video0 \
+--set-ctrl=brightness=120 \
+--set-ctrl=contrast=32 \
+--set-ctrl=saturation=58 \
+--set-ctrl=white_balance_temperature_auto=0 \
+--set-ctrl=white_balance_temperature=4500\
+--set-ctrl=gain=128 \
+--set-ctrl=power_line_frequency=2 \
+--set-ctrl=sharpness=22 \
+--set-ctrl=backlight_compensation=1 \
+--set-ctrl=exposure_auto_priority=0 \
+--set-ctrl=exposure_auto=1 \
+--set-ctrl=exposure_absolute=60 \
+--set-ctrl=focus_auto=0 \
+--set-ctrl=focus_absolute=0 \
+--set-ctrl=zoom_absolute=1
+
+v4l2-ctl -d /dev/video1 \
+--set-ctrl=brightness=120 \
+--set-ctrl=contrast=32 \
+--set-ctrl=saturation=58 \
+--set-ctrl=white_balance_temperature_auto=0 \
+--set-ctrl=white_balance_temperature=4500\
+--set-ctrl=gain=128 \
+--set-ctrl=power_line_frequency=2 \
+--set-ctrl=sharpness=22 \
+--set-ctrl=backlight_compensation=1 \
+--set-ctrl=exposure_auto_priority=0 \
+--set-ctrl=exposure_auto=1 \
+--set-ctrl=exposure_absolute=60 \
+--set-ctrl=focus_auto=0 \
+--set-ctrl=focus_absolute=0 \
+--set-ctrl=zoom_absolute=1
