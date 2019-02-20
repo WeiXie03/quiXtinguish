46f1be0ffdfcf20da85c76294c9e26cee728ba9a
Wei Xie@RPi <wei.tom.xie@gmail.com>
2019-02-19 18:11:05 -0800

    removed some old servo and motor stuff

--- rpi/testMotors.py
+++ rpi/testMotors.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as gpio
-gpio.setmode(gpio.BOARD)
-
 
 class Motor():
-    def __init__(self, in1, in2, enable, frequency):
+    def __init__(self, in1, in2, enable, frequency=100, name='default'):
         #both sides of L293D have pair of connections to motor and an enable controlled by RPi
         self.in1_pin = in1
         self.in2_pin = in2
         self.enable_pin = enable
         self.freq = frequency
         self.pwm = self.setupPins()
+        self.name = name
+
+    def __repr__(self):
+        return self.name
 
     def setupPins(self):
         gpio.setup(self.in1_pin, gpio.OUT)
@@ -45,30 +47,31 @@ class Motor():
         self.setSpeed(dutCyc)
 
 if __name__ == "__main__":
-
+    gpio.setmode(gpio.BOARD)
     FREQUENCY = 120#Hz
     #in1, in2, enable, frequency
-    left = Motor(16, 18, 33, FREQUENCY)
-    #right = Motor(, FREQUENCY)
+    left = Motor(16, 18, 33, FREQUENCY, 'left')
+    right = Motor(13, 15, 7, FREQUENCY, 'right')
 
     while True:
-        #for motor in (left, right):
-        try:
-            direction = input("Which way?[f/b]: ")
-            speed = float(input('How fast(portion of maximum speed in percentage)? '))
-            if direction == 'f':
-                left.clockwise(speed)
-            else:
-                print('back')
-                left.counterclockwise(speed)
-        except:
-            print('no')
-            raise
-            gpio.output(left.in1_pin, False)
-            gpio.output(left.in2_pin, False)
-            #stop the pwm signal
-            left.pwm.stop()
-            gpio.output(left.enable_pin, False)
-            break
+        for motor in (left, right):
+            print(motor)
+            try:
+                direction = input("Which way?[f/b]: ")
+                speed = float(input('How fast(portion of maximum speed in percentage)? '))
+                if direction == 'f':
+                    motor.clockwise(speed)
+                else:
+                    print('back')
+                    motor.counterclockwise(speed)
+            except:
+                print('no')
+                raise
+                gpio.output(left.in1_pin, False)
+                gpio.output(left.in2_pin, False)
+                #stop the pwm signal
+                left.pwm.stop()
+                gpio.output(left.enable_pin, False)
+                break
 
     gpio.cleanup()
