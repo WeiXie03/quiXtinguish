dc856a13b1097b219a2631ab28252570c82225ea
Wei Xie@RPi <wei.tom.xie@gmail.com>
2019-02-19 20:11:17 -0800

    can now control both motors separately, switched to pigpio for software pwm

--- rpi/testMotors.py
+++ rpi/testMotors.py
@@ -1,77 +1,90 @@
-import RPi.GPIO as gpio
+import pigpio as gpio
 
 class Motor():
-    def __init__(self, in1, in2, enable, frequency=100, name='default'):
+    def __init__(self, rpi, in1, in2, enable, frequency=100, name='default'):
+        #pigpio raspberry pi object motor will be controlled by
+        self.pi = rpi
         #both sides of L293D have pair of connections to motor and an enable controlled by RPi
         self.in1_pin = in1
         self.in2_pin = in2
+
         self.enable_pin = enable
         self.freq = frequency
-        self.pwm = self.setupPins()
+        self.pi.set_PWM_range(self.enable_pin, 255)
+        self.pwmRange = self.pi.get_PWM_range(self.enable_pin)
+
+        self.setupPins()
         self.name = name
+
+    def setupPins(self):
+        self.pi.set_mode(self.in1_pin, gpio.OUTPUT)
+        self.pi.set_mode(self.in2_pin, gpio.OUTPUT)
+
+        #pwm connected to enable for speed control
+        self.pi.set_mode(self.enable_pin, gpio.OUTPUT)
+        self.pi.set_PWM_frequency(self.enable_pin, self.freq)
+
+        print('pins set up')
 
     def __repr__(self):
         return self.name
 
-    def setupPins(self):
-        gpio.setup(self.in1_pin, gpio.OUT)
-        gpio.output(self.in1_pin, True)
-        gpio.setup(self.in2_pin, gpio.OUT)
-        gpio.output(self.in2_pin, True)
-
-        #pwm connected to enable for speed control
-        gpio.setup(self.enable_pin, gpio.OUT)
-        pwm = gpio.PWM(self.enable_pin, self.freq)
-        gpio.output(self.enable_pin, True)
-        #start the signal at 0 intensity(doesn't actually make anything happen)
-        pwm.start(0)
-        return pwm
-
-        print('pins set up')
-
     def setSpeed(self, dutCyc):
+        print('percentage of max speed ', dutCyc, '%')
+        dutCyc = int(self.pwmRange*dutCyc/100)
         #set speed using PWM to enable of L293D
         #duty cycle should be in percent
-        self.pwm.ChangeDutyCycle(dutCyc)
-        print('percentage of max speed ', dutCyc, '%')
+        self.pi.set_PWM_dutycycle(self.enable_pin, dutCyc)
 
     def clockwise(self, dutCyc):
-        gpio.output(self.in1_pin, gpio.HIGH)
-        gpio.output(self.in2_pin, gpio.LOW)
+        self.pi.write(self.in1_pin, 1)
+        self.pi.write(self.in2_pin, 0)
         self.setSpeed(dutCyc)
 
     def counterclockwise(self, dutCyc):
         #reverse direction = reverse pins
-        gpio.output(self.in1_pin, gpio.LOW)
-        gpio.output(self.in2_pin, gpio.HIGH)
+        self.pi.write(self.in1_pin, 0)
+        self.pi.write(self.in2_pin, 1)
         self.setSpeed(dutCyc)
 
+    def close(self):
+        self.pi.write(self.in1_pin, 0)
+        self.pi.write(self.in2_pin, 0)
+        self.pi.set_PWM_dutycycle(self.enable_pin, 0)
+        print('terminated signals from pins ', self.in1_pin, self.in2_pin, self.enable_pin)
+
+    '''
+    def __del__(self):
+        self.pi.write(self.in1_pin, 0)
+        self.pi.write(self.in2_pin, 0)
+        self.pi.set_PWM_dutycycle(self.enable_pin, 0)
+        print('terminated signals from pins ', self.in1_pin, self.in2_pin, self.enable_pin)
+    '''
+
 if __name__ == "__main__":
-    gpio.setmode(gpio.BOARD)
-    FREQUENCY = 120#Hz
-    #in1, in2, enable, frequency
-    left = Motor(16, 18, 33, FREQUENCY, 'left')
-    right = Motor(13, 15, 7, FREQUENCY, 'right')
+    try:
+        pi = gpio.pi()
+        FREQUENCY = 1000#Hz
+        #in1, in2, enable, frequency
+        left = Motor(pi, 23, 24, 13, FREQUENCY, 'left')
+        right = Motor(pi, 27, 22, 17, FREQUENCY, 'right')
 
-    while True:
-        for motor in (left, right):
-            print(motor)
-            try:
-                direction = input("Which way?[f/b]: ")
-                speed = float(input('How fast(portion of maximum speed in percentage)? '))
-                if direction == 'f':
-                    motor.clockwise(speed)
-                else:
-                    print('back')
-                    motor.counterclockwise(speed)
-            except:
-                print('no')
-                raise
-                gpio.output(left.in1_pin, False)
-                gpio.output(left.in2_pin, False)
-                #stop the pwm signal
-                left.pwm.stop()
-                gpio.output(left.enable_pin, False)
-                break
-
-    gpio.cleanup()
+        while True:
+            for motor in (left, right):
+                print(motor)
+                try:
+                    direction = input("Which way?[f/b]: ")
+                    speed = float(input('How fast(portion of maximum speed in percentage)? '))
+                    if direction == 'f':
+                        motor.clockwise(speed)
+                    else:
+                        print('back')
+                        motor.counterclockwise(speed)
+                except:
+                    print('no')
+                    raise
+                    pi.stop()
+                    break
+    finally:
+        left.close()
+        right.close()
