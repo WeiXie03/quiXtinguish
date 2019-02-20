142252799709451aeb2ddf6fdac5994728e0eca5
Cheng Xie <cheng.k.xie@gmail.com>
2019-02-20 01:20:48 -0800

    adding some image data collected some time ago, started on motor and servo control server script

--- rpi/testServo.py
+++ rpi/testServo.py
@@ -1,15 +1,7 @@
-# uncompyle6 version 3.2.5
-# Python bytecode 2.7 (62211)
-# Decompiled from: Python 3.5.3 (default, Sep 27 2018, 17:25:39)
-# [GCC 6.3.0 20170516]
-# Embedded file name: /home/pi/Public/rpi/testServo.py
-# Compiled at: 2019-02-16 16:09:25
 import RPi.GPIO as gpio
 from time import sleep
-gpio.setmode(gpio.BOARD)
 
 class pwmServo():
-
     def __init__(self, pin, freq):
         self.pin = pin
         gpio.setup(self.pin, gpio.OUT)
@@ -28,6 +20,7 @@ class pwmServo():
 
 
 if __name__ == '__main__':
+    gpio.setmode(gpio.BOARD)
     tilt = pwmServo(32, 60)
     for it in range(50):
         try:
