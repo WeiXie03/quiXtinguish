067fa501e391a31089c6b75aaa24256bb816eb21
Wei Xie <wei.tom.xie@gmail.com>
2019-02-25 08:13:06 -0800

    got a video stream working and lists containing parameters to control the robot's servos and motors being generated

--- motiveClient.py
+++ motiveClient.py
@@ -1,38 +1,78 @@
+import sys
 import socket
-import testServo
-import RPi.GPIO as GPIO
+import numpy as np
+import cv2
+import keyboard as kb
+from data_collection.receivewrite2 import *
+from time import sleep
 
-HOST = '192.168.0.16'
+HOST = input('Enter IP address of RPi: ')
 PORT = 3027
+TIMEOUT_SECONDS = 1e-3
 
-#numbering scheme is physical board
-TILT_PIN = 32
-PAN_PIN = 12
-FREQ = 60
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    tilt = testServo.pwmServo(TILT_PIN, FREQ)
-    pan = testServo.pwmServo(PAN_PIN, FREQ)
-    sock.connect((HOST, PORT))
-
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    sock.settimeout(TIMEOUT_SECONDS)
+    #sock.connect((HOST, PORT))
+    print('connected to RPi at', HOST, 'on port', PORT)
+    left = Cam(5000, 'left', make_win=False)
+    right = Cam(5200, 'right', make_win=False)
+    window = cv2.namedWindow('window', cv2.WINDOW_OPENGL)
+    #cv2.startWindowThread()
+    print('?')
+    #cmds = ['fw', 0, 90, 90] #initial settings
+    curServoAngles = [90, 90]
     while True:
         try:
-            data = sock.recv(1024).decode()
-            if data:
-                #assign which servo and the angle
-                data = data.split(', ')
-                servo = data[0]
-                angle = int(data[1])
+            curServoAngles = sock.recv(1024).decode()
+        except socket.timeout as e:
+            pass
+        except socket.error as e:
+            print(e)
+            sys.exit(1)
+        else:
+            curServoAngles = curServoAngles.split(',')
 
-                print('turning ', servo, ' to ', angle)
-                if servo == 'tilt' or servo == 't':
-                    tilt.specTurn(angle)
-                elif servo == 'pan' or 'p':
-                    pan.specTurn(angle)
-        except Exception:
-            raise
-            tilt.turn.stop()
-            pan.turn.stop()
-            GPIO.cleanup()
-            print('terminating')
-            break
+        frames = []
+        for stream in (left, right):
+            _, frame = stream.cap.read()
+            frames.append(frame)
+
+        cv2.imshow('window', np.hstack(frames))
+
+        key_hit = cv2.waitKey(20)
+        '''
+        print(key_hit)
+        if not key_hit == -1:
+            sleep(0.001)
+        '''
+
+        cmds = [None, None, None, None]
+        if key_hit == ord('w'):
+            cmds[0] = 'fw'
+        elif key_hit == ord('s'):
+            cmds[0] = 'bw'
+        elif key_hit == ord('a'):
+            cmds[0] = 'l'
+        elif key_hit == ord('d'):
+            cmds[0] = 'r'
+        else:
+            cmds[0] = 'pause'
+
+        cmds[1] = 40 #setting speed to 40% of max for now
+
+        targetAngles = curServoAngles.copy()
+        if key_hit == ord('q'):
+            targetAngles[0] -= 1
+        elif key_hit == ord('e'):
+            targetAngles[0] += 1
+        elif key_hit == ord('r'):
+            targetAngles[1] += 1
+        elif key_hit == ord('f'):
+            targetAngles[1] -= 1
+
+        cmds[2] = targetAngles[0]
+        cmds[3] = targetAngles[1]
+        cmdsBytes = ','.join(str(cmds)).encode()
+
+        print(cmds)
+        #sock.sendto(cmdsBytes, (HOST, PORT))
