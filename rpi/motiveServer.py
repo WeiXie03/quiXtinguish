8703de5ae69f6d4f83adc8858d1c03ffa88aa4e1
Wei Xie <wei.tom.xie@gmail.com>
2019-02-27 10:28:18 -0800

    recoded server

--- rpi/motiveServer.py
+++ rpi/motiveServer.py
@@ -1,21 +1,100 @@
 import socket
+import numpy
+import keyboard
+import pigpio as pig
+from testMotion.testServo import *
+from testMotion.testMotors import *
+import sys
 
-HOST = input('Enter IP address of RPi. ')
-PORT = 3027
+class Robot():
+    def __init__(self, l_motor_pins=(26,19,13), r_motor_pins=(5,6,0), tilt_pin=23, pan_pin=24, frequency=50, name='robot'):
+        self.pi = pig.pi()
+        self.name = name
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind(HOST, PORT)
-    sock.listen(5)
-    conn, addr = sock.accept()
-    with conn:
-        print('Connected at ', addr)
+        self.freq = frequency
+        self.left = Motor(pi, l_motor_pins, frequency, 'left')
+        self.right = Motor(pi, r_motor_pins, frequency, 'right')
+        self.tilt = hwServo(pi, tilt_pin, frequency, 1420, 1750)
+        self.pan = Servo(pi, pan_pin, frequency)
+
+    def __repr__(self):
+        return '{} using {} pigpio raspberry pi object\nfrequency={}, left motor controlled by pins {}, right by {}, tilt servo controlled by {}, pan by {}'.format(self.name, self.pi, self.freq, (self.left.in1_pin, self.left.in2_pin, self.left.enable_pin), (self.right.in1_pin, self.right.in2_pin, self.right.enable_pin), self.tilt.pin, self.pan.pin)
+
+    def forward(self, lspeed, rspeed):
+        self.left.clockwise(self, lspeed)
+        self.right.clockwise(self, rspeed)
+
+    def backward(self, lspeed, rspeed):
+        self.left.counterclockwise(self, lspeed)
+        self.right.counterclockwise(self, rspeed)
+
+    def rotate(self, servo_angles):
+        self.tilt.specTurn(self.tilt.calc_duty(servo_angles[0]))
+        self.pan.specTurn(self.pan.calc_duty(servo_angles[1]))
+
+def parse_cmds(cmds, robot):
+    #cmds is in form ['f' or 'b', 'l' or 'r', drive speed, tilt angle, pan angle]
+
+    #motors
+    #both of class Robot's driving functions take (robot, left motor speed, right motor speed)
+    lspeed, rspeed = 0, 0
+    if cmds[0] != 'pause':
+        if cmds[1] == 'pause':
+            lspeed, rspeed = cmds[2], cmds[2]
+        elif cmds[1] == 'l':
+            lspeed, rspeed = cmds[2], cmds[2]/2
+        else:
+            lspeed, rspeed = cmds[2]/2, cmds[2]
+        #actually command the motors
+        if cmds[0] == 'f':
+            robot.forward(lspeed, rspeed)
+        else:
+            robot.backward(lspeed, rspeed)
+    else:
+        if cmds[1] == 'l':
+            lspeed, rspeed = 0, cmds[2]
+        elif cmds[1] == 'r':
+            lspeed, rspeed = cmds[2], 0
+        #elif cmds[1] is also pause, do nothing
+
+        #turn one track forward while the other does not; pivot forward
+        robot.forward(lspeed, rspeed)
+
+    #servos
+    self.rotate((cmds[3], cmds[4]))
+
+if __name__ == "__main__":
+    HOST = input('Enter IP address of RPi. ')
+    PORT = 3027
+
+    robot = Robot(name='FireBot')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.bind((HOST, PORT))
         while True:
-            print('tilt servo')
-            tilt = input('Enter angle you\'d like to turn to in degrees. ')
-            print('pan servo')
-            tilt = input('Enter angle you\'d like to turn to in degrees. ')
+            try:
+                cmds, addr = sock.recvfrom(2048)
+            except socket.timeout as e:
+                print(e)
+                print('waiting(?)')
+            except socket.error as e:
+                print(e)
+                sys.exit()
+            else:
+                cmds = [ele for ele in (cmds.decode()).split(',')]
+                for cmd in cmds[2:]:
+                    cmd = int(cmd)
 
-            print('left motor')
-            left = input('Enter percentage of maximum speed you want motors to spin at. ')
-            print('right motor')
-            left = input('Enter percentage of maximum speed you want motors to spin at. ')
+                parse_cmds(cmds, robot)
+
+                cur_servo_angles = ','.join(cmds[3:]).encode()
+                sock.sendto(cur_servo_angles, addr)
+
+            finally:
+                robot.left.close()
+                robot.right.close()
+                robot.pi.write(robot.tilt.pin, 0)
+                robot.pi.write(robot.pan.pin, 0)
+                robot.pi.stop()
+                print(robot.name, 'dead')
+                break
--- rpi/testMotion/testServo.py
+++ rpi/testMotion/testServo.py
@@ -1,28 +1,42 @@
 import pigpio as pig
 
-class pwmServo():
-    def __init__(self, pi, pin, freq):
+class Servo():
+    def __init__(self, pi, pin, freq, min_bound=1000, max_bound=2000):
         self.pin = pin
         self.pi = pi
         self.freq = freq
         self.pi.set_mode(self.pin, pig.OUTPUT)
+        self.pi.set_PWM_frequency(self.pin, freq)
+        self.pwm_range = 40000
+        self.pi.set_PWM_range(self.pin, self.pwm_range) #set default range to 10000
 
-    def specTurn(self, angle):
+    def calc_duty(self, angle):
         pulse_wid = (1000/90) * angle + 500 #using points (0 deg, 500 us), (180 deg, 2500 us), derived: pulse width = (1000us/90deg)*angle+500us, us=microseconds
-        if pulse_wid <= 1420:
-            pulse_wid = 1420
-            print('outside range of allowed angles, pulse width set to lower bound 1420 microseconds')
-        elif pulse_wid >= 1750:
-            pulse_wid = 1750
-            print('outside range of allowed angles, pulse width set to upper bound 1750 microseconds')
+        if pulse_wid <= min_bound:
+            pulse_wid = min_bound
+            print('outside range of allowed angles, pulse width set to lower bound {} microseconds').format(min_bound)
+        elif pulse_wid >= max_bound:
+            pulse_wid = max_bound
+            print('outside range of allowed angles, pulse width set to upper bound {} microseconds').format(max_bound)
 
-        duty = (pulse_wid/10**6) * self.freq #pulse width is in us, frequency is cyc/s
         print('\tpulse width', pulse_wid)
+        duty = (pulse_wid/10**6) * self.freq #pulse width is in us, frequency is cyc/s
+        return duty
 
-        #scale to pigpio's range of 0 to 1 million, pigpio only takes ints
-        duty = int(duty * 10**6)
-        print('rounded duty for pigpio =', duty)
-        self.pi.hardware_PWM(self.pin, self.freq, duty)
+    def specTurn(self, duty):
+        #scale to pigpio pwm range set, pigpio only takes ints
+        duty = int(duty * self.pwm_range)
+        #print('rounded duty for pigpio =', duty)
+        self.pi.set_PWM_dutycycle(self.pin, self.duty) #set default range to 10000
+
+class hwServo(Servo):
+    def __init__(self, pi, pin, freq, min_bound=1000, max_bound=2000)
+        super().__init__(self, pi, pin, freq, min_bound=1000, max_bound=2000)
+    def specTurn(self, duty):
+        #scale to pigpio pwm range set, pigpio only takes ints
+        duty = int(duty * self.pwm_range)
+        #print('rounded duty for pigpio =', duty)
+        self.pi.hardware_PWM(self.pin, self.freq, duty) #set default range to 10000
 
 if __name__ == '__main__':
     pi = pig.pi()
