a1bbfeed91cea0787fbe30c57f910ac42cb8a7fb
Wei Xie@RPi <wei.tom.xie@gmail.com>
2019-02-27 10:53:59 -0800

    fixed local copy of testServo

--- rpi/testMotion/testServo.py
+++ rpi/testMotion/testServo.py
@@ -1,12 +1,7 @@
 import pigpio as pig
 
-<<<<<<< HEAD
-class pwmServo():
-    def __init__(self, pi, pin, freq, lBound=1420, uBound=1750):
-=======
 class Servo():
     def __init__(self, pi, pin, freq, min_bound=1000, max_bound=2000):
->>>>>>> b9c4d2962bc93cb15cde6defcf3bf017f82d1c47
         self.pin = pin
         self.pi = pi
         self.freq = freq
@@ -22,23 +17,6 @@ class Servo():
 
     def calc_duty(self, angle):
         pulse_wid = (1000/90) * angle + 500 #using points (0 deg, 500 us), (180 deg, 2500 us), derived: pulse width = (1000us/90deg)*angle+500us, us=microseconds
-<<<<<<< HEAD
-        if pulse_wid <= self.lBound:
-            pulse_wid = self.lBound
-            #print('outside range of allowed angles, pulse width set to lower bound {} microseconds'.format(lBound))
-        elif pulse_wid >= self.uBound:
-            pulse_wid = self.uBound
-            #print('outside range of allowed angles, pulse width set to upper bound {} microseconds'.format(uBound))
-
-        duty = (pulse_wid/10**6) * self.freq #pulse width is in us, frequency is cyc/s
-        #print('\tpulse width', pulse_wid)
-
-        #scale to pigpio's range of 0 to 1 million, pigpio only takes ints
-        duty = int(duty * 10**6)
-        #print('rounded duty for pigpio =', duty)
-        self.pi.hardware_PWM(self.pin, self.freq, duty)
-        self.cur_angle = angle
-=======
         if pulse_wid <= min_bound:
             pulse_wid = min_bound
             print('outside range of allowed angles, pulse width set to lower bound {} microseconds').format(min_bound)
@@ -64,7 +42,6 @@ class hwServo(Servo):
         duty = int(duty * self.pwm_range)
         #print('rounded duty for pigpio =', duty)
         self.pi.hardware_PWM(self.pin, self.freq, duty) #set default range to 10000
->>>>>>> b9c4d2962bc93cb15cde6defcf3bf017f82d1c47
 
 if __name__ == '__main__':
     pi = pig.pi()
