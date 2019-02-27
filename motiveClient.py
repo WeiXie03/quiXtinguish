050a9a17bebfe585e7ab822aa5e59a7585ce09b8
Wei Xie <wei.tom.xie@gmail.com>
2019-02-26 19:40:56 -0800

    modified the remote control client to use the keyboard module instead of OpenCV's GUI stuff

--- motiveClient.py
+++ motiveClient.py
@@ -2,9 +2,41 @@ import sys
 import socket
 import numpy as np
 import cv2
-import keyboard as kb
+import keyboard
 from data_collection.receivewrite2 import *
 from time import sleep
+
+#NOTE: OpenCV in Python just cannot handle video streaming and keyboard control at the same time, will just manually run receiverwrite2.py concurrently
+
+def drive():
+    cmd = ['','']
+    if keyboard.is_pressed('w'):
+        cmd[0] = 'f'
+    elif keyboard.is_pressed('s'):
+        cmd[0] = 'b'
+    else:
+        cmd[0] = 'pause'
+
+    if keyboard.is_pressed('a'):
+        cmd[1] = 'l'
+    elif keyboard.is_pressed('d'):
+        cmd[1] = 'r'
+    else:
+        cmd[1] = 'pause'
+
+    return cmd
+
+def rotate(targetAngles):
+    #targetAngles is [horizontal(panning), vertical(tilting)]
+    if keyboard.is_pressed('j'):
+        targetAngles[0] -= 1
+    elif keyboard.is_pressed('l'):
+        targetAngles[0] += 1
+    elif keyboard.is_pressed('i'):
+        targetAngles[1] += 1
+    elif keyboard.is_pressed('k'):
+        targetAngles[1] -= 1
+    return targetAngles
 
 HOST = input('Enter IP address of RPi: ')
 PORT = 3027
@@ -14,12 +46,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     sock.settimeout(TIMEOUT_SECONDS)
     #sock.connect((HOST, PORT))
     print('connected to RPi at', HOST, 'on port', PORT)
-    left = Cam(5000, 'left', make_win=False)
-    right = Cam(5200, 'right', make_win=False)
-    window = cv2.namedWindow('window', cv2.WINDOW_OPENGL)
-    #cv2.startWindowThread()
-    print('?')
-    #cmds = ['fw', 0, 90, 90] #initial settings
+
     curServoAngles = [90, 90]
     while True:
         try:
@@ -32,46 +59,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         else:
             curServoAngles = curServoAngles.split(',')
 
-        frames = []
-        for stream in (left, right):
-            _, frame = stream.cap.read()
-            frames.append(frame)
+        cmds = [None, None, None, None, None]
 
-        cv2.imshow('window', np.hstack(frames))
-
-        key_hit = cv2.waitKey(20)
-        '''
-        print(key_hit)
-        if not key_hit == -1:
-            sleep(0.001)
-        '''
-
-        cmds = [None, None, None, None]
-        if key_hit == ord('w'):
-            cmds[0] = 'fw'
-        elif key_hit == ord('s'):
-            cmds[0] = 'bw'
-        elif key_hit == ord('a'):
-            cmds[0] = 'l'
-        elif key_hit == ord('d'):
-            cmds[0] = 'r'
-        else:
-            cmds[0] = 'pause'
-
-        cmds[1] = 40 #setting speed to 40% of max for now
+        #setting speed to 40% of max for now
+        cmds[0], cmds[1] = drive()
+        cmds[2] = 40
 
         targetAngles = curServoAngles.copy()
-        if key_hit == ord('q'):
-            targetAngles[0] -= 1
-        elif key_hit == ord('e'):
-            targetAngles[0] += 1
-        elif key_hit == ord('r'):
-            targetAngles[1] += 1
-        elif key_hit == ord('f'):
-            targetAngles[1] -= 1
+        targetAngles = rotate(targetAngles)
+        cmds[3], cmds[4] = targetAngles
+        cmds[3] = numpy.clip(cmds[3], 0, 180)
+        cmds[4] = numpy.clip(cmds[3], 0, 180)
 
-        cmds[2] = targetAngles[0]
-        cmds[3] = targetAngles[1]
         cmdsBytes = ','.join(str(cmds)).encode()
 
         print(cmds)
