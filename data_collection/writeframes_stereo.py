d48a4c54eb3be9c483ad348dfdc757412aee521b
Wei Xie <wei.tom.xie@gmail.com>
2019-03-17 00:30:45 -0700

    Finally got a near infrared camera for the RPi, modified receivewrite2(now writeframes) for nir

---
+++ data_collection/writeframes_stereo.py
@@ -0,0 +1,73 @@
+import numpy
+import cv2
+import os
+import pickle
+
+class Cam():
+    def __init__(self, port, side, make_win=True):
+        self.side = side
+        #self.cap = cv2.VideoCapture('udpsrc port={} ! application/x-rtp,media=video,payload=26,encoding-name=JPEG ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink'.format(port), cv2.CAP_GSTREAMER)
+        #video streaming from RPi to local stdin via netcat now
+        self.cap = cv2.VideoCapture('/dev/stdin')
+        print("Finished getting cap")
+        if make_win:
+            self.win = cv2.namedWindow(self.side, cv2.WINDOW_OPENGL)
+
+if __name__ == "__main__":
+    DATA_DIR = 'data/nir_test/'
+    print('Setting up cams')
+    streaml = Cam(5200, 'left')
+    streamr = Cam(5000, 'right')
+    print('Setup complete')
+
+    metadata_path = os.path.join(DATA_DIR, 'metadata.dat')
+    metadata = None
+    if os.path.isfile(metadata_path):
+        with open(metadata_path, 'rb') as metadataf:
+            metadata = pickle.load(metadataf)
+    else:
+        metadata = {}
+
+    im_count = 0
+    if len(metadata.keys()) > 0:
+        im_count = max(metadata.keys()) + 1
+
+    print('Starting')
+    while(True):
+        key_hit = None
+        for stream in (streaml, streamr):
+            print('Waiting')
+            _, frame = stream.cap.read()
+            cv2.imshow(stream.side, frame)
+        key_hit = cv2.waitKey(1)
+
+        if key_hit == 32: #space bar
+            # Want these to happen consecutive
+            _, framel = streaml.cap.read()
+            _, framer = streamr.cap.read()
+
+            metadata_entry = {}
+            for stream, frame  in [(streaml, framel), (streamr, framer)]:
+                metadata_entry[stream.side] = {}
+                img_path = os.path.join(DATA_DIR, stream.side,
+                        "{}.jpg".format(str(im_count)))
+                metadata_entry[stream.side]["img_path"] = img_path
+                cv2.imwrite(img_path, frame)
+                print('wrote ', img_path, im_count)
+            im_count += 1
+
+            metadata[im_count-1] = metadata_entry
+
+            with open(metadata_path+'d', 'wb') as metadataf:
+                pickle.dump(metadata, metadataf)
+
+        elif key_hit == ord('q'):
+            break
+
+    with open(metadata_path, 'wb') as metadataf:
+        pickle.dump(metadata, metadataf)
+
+    #When everything done, release the capture
+    for stream in (streaml, streamr):
+        stream.cap.release()
+    cv2.destroyAllWindows()
