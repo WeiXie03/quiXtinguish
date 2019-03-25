c0e18e8a69ff03708ae1cc6a32df144ede021138
Wei Xie <wei.tom.xie@gmail.com>
2019-03-25 12:34:25 -0700

    added NIR-only image collecting script

---
+++ data_collection/writeframes_nir.py
@@ -0,0 +1,67 @@
+import numpy
+import cv2
+import os
+import pickle
+
+class Cam():
+    def __init__(self, port, name, make_win=True):
+        self.name = name
+        self.cap = cv2.VideoCapture('udpsrc port={} ! application/x-rtp,payload=96,media=video,encoding-name=H264 ! rtph264depay ! decodebin ! videoconvert ! appsink'.format(port), cv2.CAP_GSTREAMER)
+        #video streaming from RPi to local stdin via netcat now
+        #self.cap = cv2.VideoCapture("/dev/stdin")
+        #self.cap = cv2.VideoCapture("udp://127.0.0.1:{}".format(port))
+        print("Finished getting cap")
+        if make_win:
+            self.win = cv2.namedWindow(self.name, cv2.WINDOW_OPENGL)
+
+if __name__ == "__main__":
+    DATA_DIR = '../quiXtinguish/data_collection/data/nir_test'
+    print('Setting up cams')
+    stream = Cam(6300, 'NoIR')
+    print('Setup complete')
+
+    metadata_path = os.path.join(DATA_DIR, 'metadata.dat')
+    metadata = None
+    if os.path.isfile(metadata_path):
+        with open(metadata_path, 'rb') as metadataf:
+            metadata = pickle.load(metadataf)
+    else:
+        metadata = {}
+
+    im_count = 0
+    if len(metadata.keys()) > 0:
+        im_count = max(metadata.keys()) + 1
+
+    print('Starting')
+    while(True):
+        key_hit = None
+        _, frame = stream.cap.read()
+        cv2.imshow(stream.name, frame)
+        key_hit = cv2.waitKey(1)
+
+        if key_hit == 32: #space bar
+            metadata_entry = {}
+            metadata_entry[stream.name] = {}
+            img_path = os.path.join(DATA_DIR, stream.name,
+                    "{}.jpg".format(im_count))
+            metadata_entry[stream.name]["img_path"] = img_path
+            cv2.imwrite(img_path, frame)
+            print('wrote ', img_path, im_count)
+
+            metadata[im_count] = metadata_entry
+            im_count += 1
+
+            '''
+            with open(metadata_path+'d', 'wb') as metadataf:
+                pickle.dump(metadata, metadataf)
+            '''
+
+        elif key_hit == ord('q'):
+            break
+
+    with open(metadata_path, 'wb') as metadataf:
+        pickle.dump(metadata, metadataf)
+
+    #When everything done, release the capture
+    stream.cap.release()
+    cv2.destroyAllWindows()
