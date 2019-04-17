0050add892b24b95879a49b39d2e5d9d020e758c
Wei Xie <wei.tom.xie@gmail.com>
2019-04-17 08:21:58 -0700

    updated brightest script to work with new camera set up

--- brightest.py
+++ brightest.py
@@ -5,6 +5,7 @@ import data_collection.writeframes as stream
 import click2pan as direc
 import socket
 import math
+from time import sleep
 
 class Window():
     def __init__(self, cam_mtx, angle_step=0.5, srch_rad=29):
@@ -38,23 +39,29 @@ class Window():
         return req_angle
     '''
 
-    def cmd_pan(self, img, coords):
-        x = coords[0]
-        print(x-self.angstep)
-        if x < img.shape[1]/2-5:
-            print(1/8+abs(x)/(img.shape[1]/32))
-            return max(-1*self.angstep*(1/8+abs(x)/(img.shape[1]/8)), -15)
-        elif x > img.shape[1]/2+5:
-            print(1/8+abs(x)/(img.shape[1]/64))
-            return min(self.angstep*(1/8+abs(x)/(img.shape[1]/8)), 15)
+    def cmd_pan(self, lcoords, rcoords, imwid):
+        lx = imwid - lcoords[0]
+        #for left, distance from right edge of window, opposite of right
+        rx = rcoords[0]
+
+        if lx < rx:
+            print('going right')
+            print(1/8+abs(rx-lx)/(imwid/32))
+            return max(-1*self.angstep*(1/8+abs(rx-lx)/(imwid/8)), -15)
+        elif lx > rx:
+            print('going left')
+            print(1/8+abs(lx-rx)/(imwid/64))
+            return min(self.angstep*(1/8+abs(lx-rx)/(imwid/8)), 15)
             #return max(self.angstep)
         else:
             return 0
 
     def calc_depth(self, lcoords, rcoords, baseline):
         foclx = self.cam_mtx[0][0]
+        print('focal len', foclx)
         x_l, x_r = lcoords[0], rcoords[0]
-        depth = (baseline*foclx)/(x_r-x_l)
+        print(x_l-x_r)
+        depth = (baseline*foclx)/(x_l-x_r)
         return depth
 
 def compare(prev, cur, subtractor):
@@ -94,14 +101,13 @@ if __name__ == "__main__":
     BRIGHT_RADIUS = 29 #pixels
     CALIB_PATH = sys.argv[2]
     #HOST = input('IP of RPi: ')
-    HOST = "192.168.43.16"
+    HOST = "192.168.0.12"
     PORT = 3030
 
-    BASELINE = 35.9/10**2
+    BASELINE = 22.2/100.0 #?, TODO: confirm baseline
 
-    nir = stream.Cam(6300, 'NoIR')
-    left = stream.Cam(5200, 'left')
-    right = stream.Cam(5000, 'right')
+    left = stream.Cam(5400, 'left')
+    right = stream.Cam(5800, 'right')
     print('cameras set up')
 
     #load the metadata, holds img paths and other things in dictionaries
@@ -119,32 +125,16 @@ if __name__ == "__main__":
 
         key_hit = None
         while(key_hit != ord('q')):
-            _, nir_frame = nir.cap.read()
             _, lframe = left.cap.read()
             _, rframe = right.cap.read()
             #print('updated')
 
             key_hit = cv2.waitKey(1)
-            '''
-            if key_hit == 32: #space bar
-                for ind in range(4):
-                # Want these to happen consecutive
-                    _, frame = nir.cap.read()
-                    w_, wastefr = nir.cap.read()
 
-                    metadata_entry = {}
-                    metadd(metadata_entry, nir.label, DATA_DIR, im_count, frame)
-                    metadata[im_count] = metadata_entry
-
-                    im_count += 1
-
-                    #backup
-                    with open(metadata_path+'d', 'wb') as metadataf:
-                        pickle.dump(metadata, metadataf)
-            '''
-
-            bspot = win.find_brightest(nir_frame)
-            win.show_spot(nir.label, nir_frame, bspot)
+            lspot = win.find_brightest(lframe)
+            rspot = win.find_brightest(rframe)
+            win.show_spot(left.label, lframe, lspot)
+            win.show_spot(right.label, rframe, rspot)
             #find brightest point, show it on current frame
 
             try:
@@ -152,7 +142,7 @@ if __name__ == "__main__":
                 cur_ang = float(cur_ang.decode())
                 print(cur_ang)
             except BlockingIOError as e:
-                #print(e)
+                print(e)
                 continue
             except ValueError:
                 print(ValueError)
@@ -160,28 +150,36 @@ if __name__ == "__main__":
             else:
                 #calculate and send angle to pan to to RPi
 
+                if abs(lspot[0] - rspot[0]) < 5:
+                    print('good enough')
+                    break
+                incre = win.cmd_pan(lspot, rspot, lframe.shape[1])
+                print('calculated')
+                sock.sendto(str(cur_ang+incre).encode(), (HOST, PORT))
+                print('sent to', (HOST, PORT))
+
+                '''
                 print(cur_ang)
                 try:
-                    while True:
+                    while(True):
                         cur_ang, (HOST, PORT) = sock.recvfrom(1024)
                         cur_ang = float(cur_ang.decode())
                 except BlockingIOError as e:
                     pass
 
                 for i in range(15):
-                    nir.cap.read()
-                _, frame = nir.cap.read()
+                    sleep(2)
+                print('frames wasted')
+                _, lframe = left.cap.read()
+                _, rframe = right.cap.read()
 
-                print(cur_ang)
+                #print(cur_ang)
                 #print('updated')
 
-                bspot = win.find_brightest(frame)
-                #check whether fire is in +/- 2 px of middle of view/frame
-                if bspot[0]<frame.shape[1]/2+2 and bspot[0]>frame.shape[1]/2-2:
-                    print('good enough')
-                    break
-                incre = win.cmd_pan(frame, bspot)
-                sock.sendto(str(cur_ang-incre).encode(), (HOST, PORT))
+                lspot = win.find_brightest(lframe)
+                rspot = win.find_brightest(rframe)
+                #check whether fire is in +/- 5 px
+                '''
 
         '''
         for cam in left, right:
@@ -202,20 +200,19 @@ if __name__ == "__main__":
         while(key_hit != ord('q')):
             _, lframe = left.cap.read()
             _, rframe = right.cap.read()
-            _, nirframe = nir.cap.read()
 
+            #stereo vision
             print(win.calc_depth(win.find_brightest(lframe), win.find_brightest(rframe), BASELINE), 'meters')
 
             key_hit = cv2.waitKey(1)
             if key_hit == 32:
 
-                metadata_entry = {left.label:{'img_paths':[]}, right.label:{'img_paths':[]}, nir.label:{'img_paths':[]}}
+                metadata_entry = {left.label:{'img_paths':[]}, right.label:{'img_paths':[]}}
                 for frame_count in range(4):
                     _, lframe = left.cap.read()
                     _, rframe = right.cap.read()
-                    _, nirframe = nir.cap.read()
 
-                    for cam, frame in (left, lframe), (nir, nirframe), (right, rframe):
+                    for cam, frame in (left, lframe), (right, rframe):
                         #path will be something like "data/dataset/45_2_left.jpg"
                         img_path = os.path.join(DATA_DIR, "{}_{}_{}.jpg".format(str(exp_count), str(frame_count), cam.label))
                         print(img_path)
@@ -229,12 +226,11 @@ if __name__ == "__main__":
                     for _ in range(DELAY_FRAMES):
                         _, _ = left.cap.read()
                         _, _ = right.cap.read()
-                        _, _ = nir.cap.read()
 
                 metadata[exp_count] = metadata_entry
                 exp_count += 1
 
-            for cam, frame in (left, lframe), (right, rframe), (nir, nirframe):
+            for cam, frame in (left, lframe), (right, rframe):
 
                 #diff = compare(cam.prev, frame, kompar)
                 #cv2.imshow(cam.label+' diff', diff)
@@ -255,6 +251,6 @@ if __name__ == "__main__":
         pickle.dump(metadata, metadataf)
 
     #When everything done, release the capture
-    for cam in nir, left, right:
+    for cam in left, right:
         cam.cap.release()
     cv2.destroyAllWindows()
