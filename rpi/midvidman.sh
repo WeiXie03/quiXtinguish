2b141781e4e7461179f84984032d6404fee0290b
Wei Xie@RPi <wei.tom.xie@gmail.com>
2019-04-21 16:48:44 -0700

    fixed some camera settings to achieve higher uniformity

--- rpi/midvidman.sh
+++ rpi/midvidman.sh
@@ -13,6 +13,6 @@ echo right cam port=5800
 RPORT=5800
 
 #Forwarding a gstream and sending one from an 3rd party RPi NoIR Camera Module
-raspivid -n -t 0 -b 3000000 -fps 18 -w 640 -h 480 -rot 90 -cd MJPEG -o - | gst-launch-1.0 fdsrc ! jpegparse ! rtpjpegpay pt=96 ! udpsink host=$HOST port=$RPORT &
+raspivid -n -t 0 -b 3250000 -fps 16 -w 640 -h 480 -cd MJPEG -ex antishake -ifx none -awb sun -co 5 -sh 5 -br 50 -o - | gst-launch-1.0 fdsrc ! jpegparse ! rtpjpegpay pt=96 ! udpsink host=$HOST port=$RPORT &
 
 gst-launch-1.0 udpsrc port=$LPORT ! udpsink host=$HOST port=$LPORT
